d6bd4201b6a5a20843867752b34ad58f5da4a176
Christian Spreng <c.spreng@zoho.com>
2025-11-09 00:15:33 +0100

    Added gitignore / update next gem selection

--- bot.py
+++ bot.py
@@ -2,6 +2,9 @@
 import sys, json, random
 
 DEBUG = False
+MAX_DISTANCE = 15
+
+# PS C:\Users\cspre\hidden-gems> ruby .\runner.rb -s 17eqmwy ..\found_gems\    
 
 random.seed(1)
 class gem_searcher:
@@ -10,11 +13,19 @@ class gem_searcher:
         __self__.width = 0
         __self__.height = 0
         __self__.first_tick = True
+        __self__.counter = 0
+        __self__.rect_path = None
     def log(__self__,message):
         if DEBUG:
             print(message,file=sys.stderr,flush=True)
 
+    def store(__self__,value):
+        __self__.counter += 1
+        if DEBUG:
+            with open(f"bot_state{__self__.counter:05}.json","w") as f:
+                json.dump(value,f)
     def analyse_json(__self__,data):
+        __self__.store(data)
         if __self__.first_tick:
             __self__.log('First Tick')
             __self__.config = data.get("config", {})
@@ -22,28 +33,34 @@ class gem_searcher:
             __self__.height = __self__.config.get("height")
             print(f"Overflys bot searching for gems on a {__self__.width}x{__self__.height} map",
                 file=sys.stderr, flush=True)
+            __self__.rect_path = __self__.calc_rectangle(__self__.width,__self__.height)
             __self__.first_tick = False
         else:
             __self__.log('Subsequent Tick')
         my_pos = {'bot':data['bot'],"visible_gems":data.get("visible_gems",None)}
         if not my_pos["visible_gems"]:
+            pos = __self__.rect_path[__self__.counter % len(__self__.rect_path)]
             my_pos["visible_gems"] = [
-                {'position':[__self__.width//2,__self__.height//2],
+                {'position':[pos[0],pos[1]],
                 'ttl':1}
             ]
         gems = [{
         'x_gem':  x['position'][0],
-        'y_gem': x['position'][1]
+        'y_gem': x['position'][1],
+        'ttl': x['ttl']
         } for x in my_pos["visible_gems"]
         ]
         x_bot = my_pos["bot"][0]
         y_bot = my_pos["bot"][1]
         for x in gems:
-            x['distance'] = abs(x_bot - x['x_gem']) +abs(y_bot-x['y_gem']) 
+            x['distance'] =  abs(x_bot - x['x_gem']) +abs(y_bot-x['y_gem']) 
         return {'x':x_bot,'y':y_bot}, gems,my_pos
     def select_gem(__self__,gems) -> tuple[int,int]:
-        gems.sort(key=lambda x:x['distance'])
-        return gems[0]['x_gem'], gems[0]['y_gem']
+        dist_sort = sorted(gems,key=lambda x:x['distance'])
+        point_sort = sorted(gems,key=lambda x:x['ttl'])
+        if dist_sort[0]['distance'] <= MAX_DISTANCE:
+            return dist_sort[0]['x_gem'], dist_sort[0]['y_gem']
+        return point_sort[0]['x_gem'], point_sort[0]['y_gem']
     def main(__self__):
         for line in sys.stdin:
             data = json.loads(line)
@@ -61,5 +78,20 @@ class gem_searcher:
             print(move, flush=True)
             meta_data['selected'] = move
             __self__.log(json.dumps(meta_data))
+    def calc_rectangle(__self__,width:int,height:int)->list[tuple[int,int]]:
+        rect = []
+        w_start = width // 3
+        w_end = width - w_start
+        h_start = height // 3
+        h_end = height - h_start
+        for x in range(w_start,w_end):
+            rect.append((x,h_start))
+        for y in range(h_start,h_end):
+            rect.append((w_end,y))
+        for x in range(w_end,w_start-1,-1):
+            rect.append((x,h_end))
+        for y in range(h_end-1,h_start,-1):
+            rect.append((w_start,y))
+        return rect
 if __name__ == "__main__":
     gem_searcher().main()
