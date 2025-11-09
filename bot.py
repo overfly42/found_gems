f04a1369e21e315298fb2da4887c04b75dac9bc7
Christian Spreng <c.spreng@zoho.com>
2025-11-09 01:26:51 +0100

    Added path planing

--- bot.py
+++ bot.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 import sys, json, random
+import itertools
 
-DEBUG = False
-MAX_DISTANCE = 15
-
+DEBUG = True
+MAX_DISTANCE = 10
+MAX_GEM_FOR_PERMUTATION = 7
+#14289
 # PS C:\Users\cspre\hidden-gems> ruby .\runner.rb -s 17eqmwy ..\found_gems\    
 
 random.seed(1)
@@ -15,6 +17,8 @@ class gem_searcher:
         __self__.first_tick = True
         __self__.counter = 0
         __self__.rect_path = None
+    def calc_distance(__self__,pos1:tuple[int,int],pos2:tuple[int,int])->int:
+        return abs(pos1[0]-pos2[0]) + abs(pos1[1]-pos2[1])
     def log(__self__,message):
         if DEBUG:
             print(message,file=sys.stderr,flush=True)
@@ -33,13 +37,14 @@ class gem_searcher:
             __self__.height = __self__.config.get("height")
             print(f"Overflys bot searching for gems on a {__self__.width}x{__self__.height} map",
                 file=sys.stderr, flush=True)
-            __self__.rect_path = __self__.calc_rectangle(__self__.width,__self__.height)
+            #__self__.rect_path = __self__.calc_rectangle(__self__.width,__self__.height)
             __self__.first_tick = False
         else:
             __self__.log('Subsequent Tick')
         my_pos = {'bot':data['bot'],"visible_gems":data.get("visible_gems",None)}
         if not my_pos["visible_gems"]:
-            pos = __self__.rect_path[__self__.counter % len(__self__.rect_path)]
+            #pos = __self__.rect_path[__self__.counter % len(__self__.rect_path)]
+            pos = (__self__.width //2, __self__.height //2)
             my_pos["visible_gems"] = [
                 {'position':[pos[0],pos[1]],
                 'ttl':1}
@@ -47,20 +52,34 @@ class gem_searcher:
         gems = [{
         'x_gem':  x['position'][0],
         'y_gem': x['position'][1],
-        'ttl': x['ttl']
+        'ttl': x['ttl'] - (abs(x['position'][0]-my_pos['bot'][0]) + abs(x['position'][1]-my_pos['bot'][1]))
         } for x in my_pos["visible_gems"]
         ]
         x_bot = my_pos["bot"][0]
         y_bot = my_pos["bot"][1]
         for x in gems:
-            x['distance'] =  abs(x_bot - x['x_gem']) +abs(y_bot-x['y_gem']) 
+            x['distance'] =  __self__.calc_distance(my_pos['bot'], (x['x_gem'],x['y_gem']))
+        __self__.my_pos = my_pos
         return {'x':x_bot,'y':y_bot}, gems,my_pos
     def select_gem(__self__,gems) -> tuple[int,int]:
-        dist_sort = sorted(gems,key=lambda x:x['distance'])
-        point_sort = sorted(gems,key=lambda x:x['ttl'])
-        if dist_sort[0]['distance'] <= MAX_DISTANCE:
-            return dist_sort[0]['x_gem'], dist_sort[0]['y_gem']
-        return point_sort[0]['x_gem'], point_sort[0]['y_gem']
+        if len(gems) > MAX_GEM_FOR_PERMUTATION:
+            # Sort GEMS by distance and by points. Select the one with most points. If there is 
+            dist_sort = sorted(gems,key=lambda x:x['distance'])
+            point_sort = sorted(gems,key=lambda x:x['ttl'],reverse=True)
+            if dist_sort[0]['distance'] <= MAX_DISTANCE:
+                return dist_sort[0]['x_gem'], dist_sort[0]['y_gem']
+            return point_sort[0]['x_gem'], point_sort[0]['y_gem']
+        best_order = None
+        best_dist = float('inf')
+        __self__.log(f"Calculating permutations for {len(gems)} gems")
+        for perm in itertools.permutations(gems):
+            dist = perm[0]['distance']
+            for i in range(len(perm)-1):
+                dist += __self__.calc_distance((perm[i]['x_gem'],perm[i]['y_gem']), (perm[i+1]['x_gem'],perm[i+1]['y_gem']))
+            if dist < best_dist:
+                best_dist = dist
+                best_order = perm
+        return best_order[0]['x_gem'], best_order[0]['y_gem']
     def main(__self__):
         for line in sys.stdin:
             data = json.loads(line)
