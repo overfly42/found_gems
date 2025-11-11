f61c330b948a1b7805a6e084807f65bf517dac36
Christian Spreng <c.spreng@zoho.com>
2025-11-11 22:16:23 +0100

    Slight changes in routing algrorithm to incorparate the maximum available gems

--- bot.py
+++ bot.py
@@ -2,11 +2,10 @@
 import sys, json, random
 import itertools
 
+
 DEBUG = False
 MAX_DISTANCE = 10
-MAX_GEM_FOR_PERMUTATION = 7
-#14289
-# PS C:\Users\cspre\hidden-gems> ruby .\runner.rb -s 17eqmwy ..\found_gems\    
+MAX_GEM_FOR_PERMUTATION = 8
 
 random.seed(1)
 class gem_searcher:
@@ -17,6 +16,8 @@ class gem_searcher:
         __self__.first_tick = True
         __self__.counter = 0
         __self__.rect_path = None
+        __self__.current_tick = 0
+        __self__.max_ticks = 0
     def calc_distance(__self__,pos1:tuple[int,int],pos2:tuple[int,int])->int:
         return abs(pos1[0]-pos2[0]) + abs(pos1[1]-pos2[1])
     def log(__self__,message):
@@ -35,12 +36,14 @@ class gem_searcher:
             __self__.config = data.get("config", {})
             __self__.width = __self__.config.get("width")
             __self__.height = __self__.config.get("height")
+            __self__.max_ticks = __self__.config.get("max_ticks")
             print(f"Overflys bot searching for gems on a {__self__.width}x{__self__.height} map",
                 file=sys.stderr, flush=True)
-            #__self__.rect_path = __self__.calc_rectangle(__self__.width,__self__.height)
+            __self__.rect_path = __self__.calc_rectangle(__self__.width,__self__.height)
             __self__.first_tick = False
         else:
             __self__.log('Subsequent Tick')
+            __self__.current_tick = data.get("tick")
         my_pos = {'bot':data['bot'],"visible_gems":data.get("visible_gems",None)}
         if not my_pos["visible_gems"]:
             #pos = __self__.rect_path[__self__.counter % len(__self__.rect_path)]
@@ -71,12 +74,17 @@ class gem_searcher:
             return point_sort[0]['x_gem'], point_sort[0]['y_gem']
         best_order = None
         best_dist = float('inf')
+        best_points = -1000
         __self__.log(f"Calculating permutations for {len(gems)} gems")
         for perm in itertools.permutations(gems):
             dist = perm[0]['distance']
+            points = perm[0]['ttl'] - dist
             for i in range(len(perm)-1):
                 dist += __self__.calc_distance((perm[i]['x_gem'],perm[i]['y_gem']), (perm[i+1]['x_gem'],perm[i+1]['y_gem']))
-            if dist < best_dist:
+                if dist - 1 <= __self__.max_ticks - __self__.current_tick:#Magic number to allow walk to the last tick.
+                    points += perm[i+1]['ttl'] - dist
+            if points > best_points or (points == best_points and dist < best_dist):
+                best_points = points
                 best_dist = dist
                 best_order = perm
         return best_order[0]['x_gem'], best_order[0]['y_gem']
@@ -92,7 +100,7 @@ class gem_searcher:
             elif y_gem != bot['y']:
                 move = 'S' if y_gem > bot['y'] else 'N'
             else:
-                move = random.choice(["N", "S", "E", "W"])
+                move = 'WAIT'#random.choice(["N", "S", "E", "W"])
 
             print(move, flush=True)
             meta_data['selected'] = move
