f7f2061cf1b8522f886a13b3d5df7c2d939369b7
Christian Spreng <c.spreng@zoho.com>
2025-11-13 20:43:08 +0100

    First Version with internal map

--- bot.py
+++ bot.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 import sys, json, random
 import itertools
+import numpy as np
 
 
 DEBUG = False
 MAX_DISTANCE = 10
 MAX_GEM_FOR_PERMUTATION = 8
+DECAY_FACTOR = 0.9
+USE_MAP_MOVE_SELECTOR = False
+
+#NEW 14089
 
 random.seed(1)
 class gem_searcher:
@@ -55,7 +60,8 @@ class gem_searcher:
         gems = [{
         'x_gem':  x['position'][0],
         'y_gem': x['position'][1],
-        'ttl': x['ttl'] - (abs(x['position'][0]-my_pos['bot'][0]) + abs(x['position'][1]-my_pos['bot'][1]))
+        # 'ttl': x['ttl'] - (abs(x['position'][0]-my_pos['bot'][0]) + abs(x['position'][1]-my_pos['bot'][1]))
+        'ttl': x['ttl']
         } for x in my_pos["visible_gems"]
         ]
         x_bot = my_pos["bot"][0]
@@ -90,19 +96,44 @@ class gem_searcher:
                 best_dist = dist
                 best_order = perm
         return best_order[0]['x_gem'], best_order[0]['y_gem']
+
+    def select_move(__self__,map:np.ndarray)->str:
+        bot_x = __self__.my_pos['bot'][0]
+        bot_y = __self__.my_pos['bot'][1]
+        directions = {}
+        directions[map[bot_y,max(bot_x-1,0)]] = 'W'
+        directions[map[bot_y,min(bot_x+1,__self__.width-1)]] = 'E'
+        directions[map[max(bot_y-1,0),bot_x]] = 'N'
+        directions[map[min(bot_y+1,__self__.height),bot_x]] = 'S'
+        __self__.log(f'Bot position: {bot_x},{bot_y} {directions}')
+        if len(directions) <= 1:
+            return None
+        best_dir = max(directions.keys(),key=lambda x:np.sum(x))
+        return directions[best_dir]
+    def select_move_old(__self__,bot,gems)->str:
+        __self__.log('Using old move selector')
+        x_gem, y_gem = __self__.select_gem(gems) 
+
+        if x_gem != bot['x']:
+            move = 'E' if x_gem > bot['x'] else 'W'
+        elif y_gem != bot['y']:
+            move = 'S' if y_gem > bot['y'] else 'N'
+        else:
+            move = 'WAIT'#random.choice(["N", "S", "E", "W"])
+        return move
     def main(__self__):
         for line in sys.stdin:
+            __self__.log('----------------')
             data = json.loads(line)
 
             bot, gems, meta_data = __self__.analyse_json(data)
-            x_gem, y_gem = __self__.select_gem(gems) 
-
-            if x_gem != bot['x']:
-                move = 'E' if x_gem > bot['x'] else 'W'
-            elif y_gem != bot['y']:
-                move = 'S' if y_gem > bot['y'] else 'N'
+            map = __self__.build_map(gems)
+            if USE_MAP_MOVE_SELECTOR:
+                move = __self__.select_move(map)
             else:
-                move = 'WAIT'#random.choice(["N", "S", "E", "W"])
+                move = None
+            if not move:
+                move = __self__.select_move_old(bot,gems)
 
             print(move, flush=True)
             meta_data['selected'] = move
@@ -122,5 +153,35 @@ class gem_searcher:
         for y in range(h_end-1,h_start,-1):
             rect.append((w_start,y))
         return rect
+
+    def build_map(__self__,gems:list(dict))->np.ndarray:
+        '''
+            Build a distance decay map for all gems
+        '''
+        full_map = np.zeros((__self__.height,__self__.width))
+        for gem in gems:
+            single_map = __self__.build_single_map(gem)
+            full_map += single_map
+        full_map[__self__.my_pos['bot'][1],__self__.my_pos['bot'][0]] = -0
+        return full_map
+    def build_single_map(__self__,gem):
+        '''
+            Build a distance decay map for a single gem. Most points are near the gem, decaying
+        '''
+        x0 = gem['x_gem']
+        y0 = gem['y_gem']
+        x = np.arange(__self__.width)
+        y = np.arange(__self__.height)[:,None]
+        distance = gem['ttl'] * DECAY_FACTOR**( np.abs(x - x0) + np.abs(y - y0))
+        return distance
 if __name__ == "__main__":
     gem_searcher().main()
+    # bot = gem_searcher()
+    # bot.width = 39
+    # bot.height = 19
+    # bot.my_pos = {'bot':[20,10]}
+    # print(bot.build_map([
+    #     {'x_gem':10,'y_gem':10,'ttl':150},
+    #     {'x_gem':15,'y_gem':15,'ttl':200},
+    #     {'x_gem':30,'y_gem':5,'ttl':250},
+    # ]))
