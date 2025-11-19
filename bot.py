979dad2a76e9716f3f2f159401e68b6d52018484
Christian Spreng <c.spreng@zoho.com>
2025-11-19 18:56:08 +0100

    Added corners for roundtrip

--- bot.py
+++ bot.py
@@ -5,10 +5,10 @@ import numpy as np
 from uuid import uuid4
 
 
-DEBUG = False
+DEBUG = True
 MAX_DISTANCE = 2
 DECAY_FACTOR = 0.8
-NUMBER_OF_LAST_POSITIONS = 20
+NUMBER_OF_LAST_POSITIONS = 5
 OPPONENT_PENALTY_TTL = 5
 
 
@@ -28,6 +28,9 @@ class gem_searcher:
         __self__.walls = None
         __self__.gems = {}
         __self__.fields = None
+        __self__.next_corner = None
+        __self__.corners = None
+        __self__.corner_index = 0
         # __self__.unseen_pos = None
         if DEBUG:
             import os
@@ -71,6 +74,7 @@ class gem_searcher:
             __self__.walls = np.ones((__self__.height,__self__.width))
             __self__.fields = np.ones((__self__.height,__self__.width))
             __self__.central_map = __self__.build_single_map({'x_gem':__self__.width//2,'y_gem':__self__.height//2,'ttl':MAX_DISTANCE})
+            __self__.corners = [ (1,1), (1,__self__.height-2), (__self__.width-2,1), (__self__.width-2,__self__.height-2) ]
         __self__.current_tick = data.get("tick")
         __self__.opponents = [x['position'] for x in data.get("visible_bots",[])]
         my_pos = {'bot':data['bot'],"visible_gems":data.get("visible_gems",None)}
@@ -79,6 +83,8 @@ class gem_searcher:
             __self__.walls[wall[1],wall[0]] = 0
         for tile in data.get('floor'):
             __self__.fields[tile[1],tile[0]] = 0.5
+        if __self__.current_tick % 50 == 0 or __self__.next_corner in data.get('wall') or __self__.next_corner in data.get('floor'):
+            __self__.corner_index += 1
         # __self__.unseen_pos = []
         # for x,y in itertools.product(range(__self__.width),range(__self__.height)):
         #     if __self__.walls[y,x] > 0.8 and __self__.fields[y,x] > 0.8:
@@ -115,7 +121,7 @@ class gem_searcher:
         '''
             Build a distance decay map for all gems
         '''
-        full_map = np.zeros((__self__.height,__self__.width))
+        full_map = np.ones((__self__.height,__self__.width))
         # Remove Gems much closer to the opponent than to us
         orderd_gems = sorted(__self__.gems.values(),key=lambda x:x['distance_to_oppeent'] - x['distance'])
         do_remove = len(__self__.opponents) > 0
@@ -125,8 +131,8 @@ class gem_searcher:
             __self__.log(f'Removed gem at {orderd_gems[0]["x_gem"]},{orderd_gems[0]["y_gem"]} as too close to opponent')
             orderd_gems.pop(0)
         # Build a map for each gem and add it to the full map
-        if not orderd_gems:
-            full_map += __self__.central_map
+        # if not orderd_gems:
+        #     full_map += __self__.central_map
         # for gem in __self__.unseen_pos:
         #     single_map = __self__.build_single_map(gem)
         #     full_map += single_map * 0.01
@@ -136,15 +142,19 @@ class gem_searcher:
         for opp in __self__.opponents:
             single_map = __self__.build_single_map({'x_gem':opp[0],'y_gem':opp[1],'ttl':OPPONENT_PENALTY_TTL})
             full_map -= single_map
+        if not __self__.gems:
+            current_corner = __self__.corners[__self__.corner_index % len(__self__.corners)]           
+            corner_map = __self__.build_single_map({'x_gem':current_corner[0],'y_gem':current_corner[1],'ttl':1})
+            full_map += corner_map
         # Set bot position to very low value to avoid selecting it
-        full_map[__self__.my_pos['bot'][1],__self__.my_pos['bot'][0]] = -0
+        # full_map[__self__.my_pos['bot'][1],__self__.my_pos['bot'][0]] = -0
         #Set all old positions to 0, to avoid going in circles
         for pos in __self__.last_positions:
             full_map[pos[1],pos[0]] = 0
         # Mask walls
         full_map = full_map * __self__.walls
-        # Set floor
-        full_map = full_map * __self__.fields
+        # # Set floor
+        # full_map = full_map * __self__.fields
         return full_map
     def select_move(__self__,map:np.ndarray)->str:
         '''
@@ -156,14 +166,27 @@ class gem_searcher:
         bot_y = __self__.my_pos['bot'][1]
         directions = {}
         #Map is indexed [y,x] Select the possible next steps
-        directions[map[bot_y,max(bot_x-1,0)]] = 'W'
-        directions[map[bot_y,min(bot_x+1,__self__.width-1)]] = 'E'
-        directions[map[max(bot_y-1,0),bot_x]] = 'N'
-        directions[map[min(bot_y+1,__self__.height),bot_x]] = 'S'
-        __self__.log(f'Bot position: {bot_x},{bot_y} {directions}')
-        best_dir = max(directions.keys(),key=lambda x:np.sum(x))
-        return directions[best_dir]
-
+        directions['W']=map[bot_y,max(bot_x-1,0)]
+        directions['E']=map[bot_y,min(bot_x+1,__self__.width-1)]
+        directions['N']=map[max(bot_y-1,0),bot_x]
+        directions['S']=map[min(bot_y+1,__self__.height),bot_x]
+        __self__.log(f'Bot position: {bot_x},{bot_y} {directions}')    
+        # if __self__.gems:
+            # best_dir = max({v:k for k,v in directions.items()}.keys(),key=lambda x:np.sum(x))
+            # return directions[best_dir]
+            # __self__.last_move = None
+        return max(directions,key=directions.get)
+        # return 'WAIT'
+#        directions = {v:k for k,v in directions.items()}
+        # if __self__.last_move and directions[__self__.last_move] > 0.1:
+        #     return __self__.last_move
+        # if 'N' in directions and directions['N'] > 0.1:
+        #     return 'N'
+        # if 'E' in directions and directions['E'] > 0.1:
+        #     return 'E'
+        # if 'S' in directions and directions['S'] > 0.1:
+        #     return 'S'
+        # return 'W'
 
 if __name__ == "__main__":
     gem_searcher().main()
